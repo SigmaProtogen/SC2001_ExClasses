fe3f973da519dd260dc9ac373fb691840c82caeb
SigmaProtogen <lewischng24@gmail.com>
2022-09-15 16:25:55 +0800

    Implementing basic functions

--- src/MergsertionSort.java
+++ src/MergsertionSort.java
@@ -5,7 +5,7 @@ import java.util.Arrays;
 public class MergsertionSort {
 
     public static void main(String[] args) {
-        int comparisons=0;
+        int keyComparisons=0;
         Scanner sc = new Scanner(System.in);
         Random rd = new Random();
         System.out.println("Enter array length");
@@ -15,41 +15,43 @@ public class MergsertionSort {
         for(int i=0;i<arr.length;i++){
             arr[i] = rd.nextInt(arrLength);
         }
-        //System.out.println(Arrays.toString(arr)); //print array
+        System.out.println(Arrays.toString(arr)); //print array
 
-        //sort array for merge and ins sorts
+        mergsertion(arr);
+
 
     }
 
+    static int S = 10;
+    public static void mergsertion(int[] arr){
+        //merge+insertion here
+    }
+
     public static void merge(int[] arr, int l, int m, int r){
-        // Find sizes of two subarrays to be merged
         int n1 = m - l + 1;
         int n2 = r - m;
 
         int[] L = new int[n1];
         int[] R = new int[n2];
-        /*Copy data to temp arrays*/
+
         for (int i = 0; i < n1; ++i)
             L[i] = arr[l + i];
         for (int j = 0; j < n2; ++j)
             R[j] = arr[m + 1 + j];
-        /* Merge the temp arrays */
-        // Initial indexes of first and second subarrays
+
         int i = 0, j = 0;
-        // Initial index of merged subarray array
         int k = l;
         while (i < n1 && j < n2) {
             if (L[i] <= R[j]) {
                 arr[k] = L[i];
                 i++;
-            }
-            else {
+            } else {
                 arr[k] = R[j];
                 j++;
             }
             k++;
         }
-        /* Copy remaining elements of L[] if any */
+
         while (i < n1) {
             arr[k] = L[i];
             i++;
@@ -61,8 +63,17 @@ public class MergsertionSort {
         }
     }
 
-    public static void insertion(){
-        //insertion sort here
+    public static void insertion(int[] arr){
+        int n = arr.length;
+        for (int i = 1; i < n; ++i) {
+            int key = arr[i];
+            int j = i - 1;
+            while (j >= 0 && arr[j] > key) {
+                arr[j + 1] = arr[j];
+                j = j - 1;
+            }
+            arr[j + 1] = key;
+        }
     }
 
     public static void sort(){
